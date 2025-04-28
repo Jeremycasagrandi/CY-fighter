35f11bcb82c1a2e1ac52c435a5c8536d420c6343
Jeremycasagrandi <jeremy.casagrandi@gmail.com>
2025-04-28 15:31:45 +0200

    aff stat perso

--- main.c
+++ main.c
@@ -4,6 +4,19 @@
 
 
 int main (){
-    creationperso();
+    Perso perso1;
+    perso1=creationperso();
+    afficherPerso(perso1);
+}
 
+
+
+void afficherPerso(Perso p) {
+    printf("\n--- Statistiques du personnage ---\n");
+    printf("Points de vie  : %d / %d\n", p.pdv, p.pdv_max);
+    printf("Attaque        : %d\n", p.attaque);
+    printf("Défense        : %d\n", p.defense);
+    printf("Agilité        : %d\n", p.agilite);
+    printf("Vitesse        : %d\n", p.vitesse);
+    printf("----------------------------------\n");
 }
